3ddac8ac812acc124b08b5b9a62d5b6b3c29de54
Konstantin Solomatov <Konstantin.Solomatov@gmail.com>
2013-12-13 20:00:56 -0500

    move views to Composites

--- model/src/main/java/jetbrains/jetpad/model/children/Composites.java
+++ model/src/main/java/jetbrains/jetpad/model/children/Composites.java
@@ -199,6 +199,12 @@ public class Composites {
     throw new IllegalArgumentException("One parameter is an ancestor of the other");
   }
 
+  public static <CompositeT extends Composite<CompositeT>> boolean isDescendant(CompositeT ancestor, CompositeT descendant) {
+    if (ancestor == descendant) return true;
+    if (descendant.parent().get() == null) return false;
+    return isDescendant(ancestor, descendant.parent().get());
+  }
+
   private static <CompositeT extends Composite<CompositeT>> List<CompositeT> reverseAncestors(CompositeT c) {
     List<CompositeT> result = toList(ancestors(c));
     Collections.reverse(result);
@@ -212,5 +218,72 @@ public class Composites {
       result.add(i);
     }
     return result;
+  }
+
+  public static <ViewT extends Composite<ViewT> & HasVisibility>
+  boolean isLastChild(ViewT v) {
+    ViewT parent = v.parent().get();
+    if (parent == null) return false;
+    List<ViewT> siblings = parent.children();
+    int index = siblings.indexOf(v);
+    for (ViewT cv : siblings.subList(index + 1, siblings.size())) {
+      if (cv.visible().get()) return false;
+    }
+    return true;
+  }
+
+  public static <ViewT extends Composite<ViewT> & HasVisibility>
+  boolean isFirstChild(ViewT cell) {
+    ViewT parent = cell.parent().get();
+    if (parent == null) return false;
+    List<ViewT> siblings = parent.children();
+    int index = siblings.indexOf(cell);
+
+    for (ViewT cv : siblings.subList(0, index)) {
+      if (cv.visible().get()) return false;
+    }
+    return true;
+  }
+
+  public static <ViewT extends Composite<ViewT> & HasFocusability & HasVisibility>
+  ViewT firstFocusable(ViewT v) {
+    return firstFocusable(v, true);
+  }
+
+  public static <ViewT extends Composite<ViewT> & HasFocusability & HasVisibility>
+  ViewT firstFocusable(ViewT v, boolean deepest) {
+    for (ViewT cv : v.children()) {
+      if (!cv.visible().get()) continue;
+      if (!deepest && cv.focusable().get()) return cv;
+
+      ViewT result = firstFocusable(cv);
+      if (result != null) return result;
+    }
+
+    if (v.focusable().get()) return v;
+
+    return null;
+  }
+
+  public static <ViewT extends Composite<ViewT> & HasFocusability & HasVisibility>
+  ViewT lastFocusable(ViewT c) {
+    return lastFocusable(c, true);
+  }
+
+  public static <ViewT extends Composite<ViewT> & HasFocusability & HasVisibility>
+  ViewT lastFocusable(ViewT v, boolean deepest) {
+    List<ViewT> children = v.children();
+    for (int i = children.size() - 1; i >= 0; i--) {
+      ViewT cv = children.get(i);
+
+      if (!cv.visible().get()) continue;
+      if (!deepest && cv.focusable().get()) return cv;
+
+      ViewT result = lastFocusable(cv, deepest);
+      if (result != null) return result;
+    }
+
+    if (v.focusable().get()) return v;
+    return null;
   }
 }
---
+++ model/src/main/java/jetbrains/jetpad/model/children/HasVisibility.java
@@ -0,0 +1,7 @@
+package jetbrains.jetpad.model.children;
+
+import jetbrains.jetpad.model.property.Property;
+
+public interface HasVisibility {
+  Property<Boolean> visible();
+}
