fed9b4788c8477d3860ed122d4297d9f8e77739a
Konstantin Solomatov <Konstantin.Solomatov@gmail.com>
2014-01-06 14:36:39 -0500

    update license text

--- model/src/main/java/jetbrains/jetpad/model/collections/set/AbstractObservableSet.java
+++ model/src/main/java/jetbrains/jetpad/model/collections/set/AbstractObservableSet.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2013 JetBrains s.r.o
+ * Copyright 2012-2014 JetBrains s.r.o
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -116,4 +116,3 @@ public abstract class AbstractObservableSet<ItemT> extends AbstractSet<ItemT> im
     return addListener(listener);
   }
 }
-
