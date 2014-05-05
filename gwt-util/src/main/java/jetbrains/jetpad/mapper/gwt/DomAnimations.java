95c70366923593e9ac3d5de8a74f4cb388be9bcd
Konstantin Solomatov <Konstantin.Solomatov@gmail.com>
2014-05-05 15:12:45 -0400

    Element -> Node

--- gwt-util/src/main/java/jetbrains/jetpad/mapper/gwt/DomAnimations.java
+++ gwt-util/src/main/java/jetbrains/jetpad/mapper/gwt/DomAnimations.java
@@ -1,6 +1,6 @@
 package jetbrains.jetpad.mapper.gwt;
 
-import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.Node;
 import com.google.gwt.query.client.Function;
 import com.google.gwt.query.client.GQuery;
 import jetbrains.jetpad.base.animation.Animation;
@@ -8,11 +8,11 @@ import jetbrains.jetpad.base.animation.Animation;
 import static com.google.gwt.query.client.GQuery.$;
 
 public class DomAnimations {
-  public static Animation fadeIn(final Element e, final int duration) {
+  public static Animation fadeIn(final Node n, final int duration) {
     return new GQueryBasedAnimation() {
       @Override
       protected GQuery createAnimation(final Runnable callback) {
-        return $(e).delay(50).hide().fadeIn(duration, new Function() {
+        return $(n).delay(50).hide().fadeIn(duration, new Function() {
           @Override
           public void f() {
             callback.run();
@@ -22,26 +22,26 @@ public class DomAnimations {
     };
   }
 
-  public static Animation fadeOut(final Element e, final int duration) {
+  public static Animation fadeOut(final Node n, final int duration) {
     return new GQueryBasedAnimation() {
       @Override
       protected GQuery createAnimation(final Runnable callback) {
-        return GQuery.$(e).css("opacity", "1").animate("opacity : 0", duration, new Function() {
+        return GQuery.$(n).css("opacity", "1").animate("opacity : 0", duration, new Function() {
           @Override
           public void f() {
             callback.run();
-            $(e).css("opacity", "1");
+            $(n).css("opacity", "1");
           }
         });
       }
     };
   }
 
-  public static Animation showSlide(final Element e, final int duration) {
+  public static Animation showSlide(final Node n, final int duration) {
     return new GQueryBasedAnimation() {
       @Override
       protected GQuery createAnimation(final Runnable callback) {
-        return $(e).delay(50).hide().slideToggle(duration, new Function() {
+        return $(n).delay(50).hide().slideToggle(duration, new Function() {
           @Override
           public void f() {
             callback.run();
@@ -51,15 +51,15 @@ public class DomAnimations {
     };
   }
 
-  public static Animation hideSlide(final Element e, final int duration) {
+  public static Animation hideSlide(final Node n, final int duration) {
     return new GQueryBasedAnimation() {
       @Override
       protected GQuery createAnimation(final Runnable callback) {
-        int height = $(e).height();
-        return $(e).height(height).animate("height : 0", duration, new Function() {
+        int height = $(n).height();
+        return $(n).height(height).animate("height : 0", duration, new Function() {
           @Override
           public void f() {
-            $(e).height(null);
+            $(n).height(null);
             callback.run();
           }
         });
