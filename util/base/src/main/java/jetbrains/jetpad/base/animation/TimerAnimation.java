a55717effcb6a95eb3fab72a3acb4ee964074c4e
Konstantin Solomatov <Konstantin.Solomatov@gmail.com>
2014-07-14 11:57:07 -0400

    implement timer based animaation

---
+++ util/base/src/main/java/jetbrains/jetpad/base/animation/TimerAnimation.java
@@ -0,0 +1,57 @@
+package jetbrains.jetpad.base.animation;
+
+import jetbrains.jetpad.base.Registration;
+import jetbrains.jetpad.base.edt.EventDispatchThread;
+
+public abstract class TimerAnimation implements Animation {
+  private DefaultAnimation myDefaultAnimation = new DefaultAnimation() {
+    @Override
+    protected void doStop() {
+    }
+  };
+  private Registration myTimerReg;
+  private int myFrame;
+  private int myMaxFrame;
+
+  protected TimerAnimation(EventDispatchThread edt, int frames, int framePeriod) {
+    if (frames <= 0) {
+      throw new IllegalArgumentException();
+    }
+
+    myTimerReg = edt.scheduleRepeating(framePeriod, new Runnable() {
+      @Override
+      public void run() {
+        onFrame();
+      }
+    });
+    myMaxFrame = frames;
+  }
+
+  @Override
+  public void stop() {
+    myFrame = myMaxFrame;
+    animateFrame(myFrame);
+    lastFrame();
+    myDefaultAnimation.stop();
+  }
+
+  private void onFrame() {
+    myFrame++;
+    animateFrame(myFrame);
+    if (myFrame == myMaxFrame) {
+      lastFrame();
+      myDefaultAnimation.done();
+    }
+  }
+
+  private void lastFrame() {
+    myTimerReg.remove();
+  }
+
+  protected abstract void animateFrame(int frame);
+
+  @Override
+  public void whenDone(Runnable r) {
+    myDefaultAnimation.whenDone(r);
+  }
+}
