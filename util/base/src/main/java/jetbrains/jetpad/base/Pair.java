cd97227a9506b9749d33c62fa9bf8ada206558d7
Konstantin Solomatov <Konstantin.Solomatov@gmail.com>
2014-08-22 10:48:04 -0400

    add pair

---
+++ util/base/src/main/java/jetbrains/jetpad/base/Pair.java
@@ -0,0 +1,31 @@
+package jetbrains.jetpad.base;
+
+public class Pair<FirstT, SecondT> {
+  public final FirstT first;
+  public final SecondT second;
+
+  public Pair(SecondT second, FirstT first) {
+    this.second = second;
+    this.first = first;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    Pair pair = (Pair) o;
+
+    if (first != null ? !first.equals(pair.first) : pair.first != null) return false;
+    if (second != null ? !second.equals(pair.second) : pair.second != null) return false;
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = first != null ? first.hashCode() : 0;
+    result = 31 * result + (second != null ? second.hashCode() : 0);
+    return result;
+  }
+}
