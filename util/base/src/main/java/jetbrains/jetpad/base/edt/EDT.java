23e12a7f959c6103ccf11e4961d3600bc812a123
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2015-04-06 18:07:44 -0400

    EDT utils

---
+++ util/base/src/main/java/jetbrains/jetpad/base/edt/EDT.java
@@ -0,0 +1,27 @@
+package jetbrains.jetpad.base.edt;
+
+import jetbrains.jetpad.base.Value;
+
+public class EDT {
+  public static Runnable validateInEDT(final EventDispatchThread validateOn, final Runnable validator) {
+    final Value<Boolean> valid = new Value<>(true);
+
+    final Runnable edtValidator = new Runnable() {
+      @Override
+      public void run() {
+        if (valid.get()) return;
+
+        validator.run();
+        valid.set(true);
+      }
+    };
+
+    return new Runnable() {
+      @Override
+      public void run() {
+        valid.set(false);
+        validateOn.schedule(edtValidator);
+      }
+    };
+  }
+}
