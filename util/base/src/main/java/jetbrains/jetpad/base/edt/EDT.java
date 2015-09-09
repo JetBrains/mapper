8ee90410294fc7dbd0592a96cccbecd6e8eb83fe
Ekaterina Frolova <ekaterina.frolova@jetbrains.com>
2015-09-09 10:34:18 +0300

    replace EDT with Edt in method names and class names except util class EDT

--- util/base/src/main/java/jetbrains/jetpad/base/edt/EDT.java
+++ util/base/src/main/java/jetbrains/jetpad/base/edt/EDT.java
@@ -3,14 +3,13 @@ package jetbrains.jetpad.base.edt;
 import jetbrains.jetpad.base.Value;
 
 public class EDT {
-  public static Runnable validateInEDT(final EventDispatchThread validateOn, final Runnable validator) {
+  public static Runnable validateInEdt(final EventDispatchThread validateOn, final Runnable validator) {
     final Value<Boolean> valid = new Value<>(true);
 
     final Runnable edtValidator = new Runnable() {
       @Override
       public void run() {
         if (valid.get()) return;
-
         validator.run();
         valid.set(true);
       }
