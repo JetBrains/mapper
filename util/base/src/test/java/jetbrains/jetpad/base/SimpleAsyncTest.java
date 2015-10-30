63be646997fa0cd81b71dee20ba52bd9023707c9
Gleb Leonov <Gleb.Leonov@jetbrains.com>
2015-10-30 20:07:46 +0300

    fix duplication in SimpleAsyncTest

--- util/base/src/test/java/jetbrains/jetpad/base/SimpleAsyncTest.java
+++ util/base/src/test/java/jetbrains/jetpad/base/SimpleAsyncTest.java
@@ -10,12 +10,7 @@ public class SimpleAsyncTest extends BaseTestCase {
 
   @Test
   public void exceptionInSuccessHandler() {
-    async.onSuccess(new Handler<Void>() {
-      @Override
-      public void handle(Void item) {
-        throw new RuntimeException();
-      }
-    });
+    async.onSuccess(throwingHandler());
     try {
       async.success(null);
     } catch (RuntimeException e) {
@@ -25,12 +20,7 @@ public class SimpleAsyncTest extends BaseTestCase {
 
   @Test
   public void exceptionInFailureHandler() {
-    async.onFailure(new Handler<Throwable>() {
-      @Override
-      public void handle(Throwable t) {
-        throw new RuntimeException();
-      }
-    });
+    async.onFailure(this.<Throwable>throwingHandler());
     try {
       async.failure(new Throwable());
     } catch (RuntimeException e) {
@@ -40,45 +30,52 @@ public class SimpleAsyncTest extends BaseTestCase {
 
   @Test(expected = IllegalStateException.class)
   public void callSuccessInSuccessHandler() {
-    async.onSuccess(new Handler<Void>() {
-      @Override
-      public void handle(Void item) {
-        async.success(null);
-      }
-    });
+    async.onSuccess(succeedingHandler(async));
     async.success(null);
   }
 
   @Test(expected = IllegalStateException.class)
   public void callSuccessInFailureHandler() {
-    async.onFailure(new Handler<Throwable>() {
-      @Override
-      public void handle(Throwable item) {
-        async.success(null);
-      }
-    });
+    async.onFailure(this.<Throwable>succeedingHandler(async));
     async.failure(new Throwable());
   }
 
   @Test(expected = IllegalStateException.class)
   public void callFailureInSuccessHandler() {
-    async.onSuccess(new Handler<Void>() {
-      @Override
-      public void handle(Void item) {
-        async.failure(null);
-      }
-    });
+    async.onSuccess(failingHandler(async));
     async.success(null);
   }
 
   @Test(expected = IllegalStateException.class)
   public void callFailureInFailureHandler() {
-    async.onFailure(new Handler<Throwable>() {
-      @Override
-      public void handle(Throwable item) {
-        async.failure(null);
-      }
-    });
+    async.onFailure(this.<Throwable>failingHandler(async));
     async.failure(new Throwable());
+  }
+
+  private <ResultT> Handler<ResultT> throwingHandler() {
+    return new Handler<ResultT>() {
+      @Override
+      public void handle(ResultT item) {
+        throw new RuntimeException();
+      }
+    };
+  }
+
+  private <ResultT> Handler<ResultT> succeedingHandler(final SimpleAsync<Void> async) {
+    return new Handler<ResultT>() {
+      @Override
+      public void handle(ResultT item) {
+        async.success(null);
+      }
+    };
+  }
+
+  private <ResultT> Handler<ResultT> failingHandler(final SimpleAsync<Void> async) {
+    return new Handler<ResultT>() {
+      @Override
+      public void handle(ResultT item) {
+        async.failure(new Throwable());
+      }
+    };
   }
 }
