bca87c652c4636145716d8f5be23ba5b5a8aa0d3
Gleb Leonov <Gleb.Leonov@jetbrains.com>
2016-02-12 16:17:29 +0300

    add TestEventDispatchThread.nothingScheduled() method for proper testing different test edts intergation

--- util/base/src/test/java/jetbrains/jetpad/base/edt/TestEventDispatchThread.java
+++ util/base/src/test/java/jetbrains/jetpad/base/edt/TestEventDispatchThread.java
@@ -30,6 +30,15 @@ public final class TestEventDispatchThread implements EventDispatchThread {
     return myRecords.isEmpty();
   }
 
+  public boolean nothingScheduled(int time) {
+    for (RunnableRecord record : myRecords) {
+      if (record.myTargetTime <= myCurrentTime + time) {
+        return false;
+      }
+    }
+    return true;
+  }
+
   public void executeUpdates() {
     executeUpdates(0);
   }
