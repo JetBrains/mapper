397b84602968fa59c0742a5d3a4b1e22f985d446
Ekaterina Frolova <ekaterina.frolova@jetbrains.com>
2016-03-03 17:30:43 +0300

    introduce with properties

---
+++ model/src/test/java/jetbrains/jetpad/model/parameters/WithMutablePropertiesTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2012-2016 JetBrains s.r.o
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrains.jetpad.model.parameters;
+
+import jetbrains.jetpad.base.Registration;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class WithMutablePropertiesTest extends WithPropertiesTestCase {
+  @Override
+  protected WithProperties createContainer() {
+    return new WithMutableProperties();
+  }
+
+  @Test
+  public void removeSetReg() {
+    Registration r = myContainer.set(myProperty, 10);
+    r.remove();
+    assertTrue(myContainer.getConfiguredProperties().isEmpty());
+  }
+
+  @Test
+  public void setValueEqualToDefaultErases() {
+    setToNotDefault();
+    myContainer.set(myProperty, myProperty.getDefaultValue(null));
+    assertTrue(myContainer.getConfiguredProperties().isEmpty());
+  }
+}
---
+++ model/src/test/java/jetbrains/jetpad/model/parameters/WithPropertiesTestCase.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2012-2016 JetBrains s.r.o
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrains.jetpad.model.parameters;
+
+import com.google.common.collect.Sets;
+import jetbrains.jetpad.base.Registration;
+import jetbrains.jetpad.test.BaseTestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public abstract class WithPropertiesTestCase extends BaseTestCase {
+  PropertySpec<Integer> myProperty = PropertySpec.of("test", 5);
+  WithProperties myContainer;
+
+  protected abstract WithProperties createContainer();
+
+  @Before
+  public void init() {
+    myContainer = createContainer();
+  }
+
+  @Test
+  public void isConfigured() {
+    PropertySpec<Integer> prop = PropertySpec.withoutDefaultValue("test");
+    assertFalse(myContainer.isConfigured(prop));
+    assertTrue(myContainer.getConfiguredProperties().isEmpty());
+    myContainer.set(prop, 1);
+    assertTrue(myContainer.isConfigured(prop));
+    assertEquals(Sets.<PropertySpec<?>>newHashSet(prop), myContainer.getConfiguredProperties());
+  }
+
+  @Test
+  public void getNotConfiguredPropWithNullDefaultValue() {
+    PropertySpec<Integer> prop = PropertySpec.of("test");
+    assertNull(myContainer.get(prop));
+  }
+
+  @Test
+  public void getNotConfiguredPropWithDefaultValue() {
+    assertEquals(5, (int) myContainer.get(myProperty));
+    assertTrue(myContainer.getConfiguredProperties().isEmpty());
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void getNotConfiguredPropWithoutDefaultValue() {
+    PropertySpec<Integer> prop = PropertySpec.withoutDefaultValue("test");
+    myContainer.get(prop);
+  }
+
+  @Test
+  public void setValueEqualToDefaultDoesNotSet() {
+    Registration reg = myContainer.set(myProperty, 5);
+    assertSame(Registration.EMPTY, reg);
+    assertTrue(myContainer.getConfiguredProperties().isEmpty());
+  }
+
+  @Test
+  public void setToNotDefault() {
+    myContainer.set(myProperty, 10);
+    assertEquals(10, (int) myContainer.get(myProperty));
+    assertEquals(Sets.<PropertySpec<?>>newHashSet(myProperty), myContainer.getConfiguredProperties());
+  }
+
+  @Test
+  public void setNullAsValue() {
+    myContainer.set(myProperty, null);
+    assertNull(myContainer.get(myProperty));
+    assertEquals(Sets.<PropertySpec<?>>newHashSet(myProperty), myContainer.getConfiguredProperties());
+  }
+}
