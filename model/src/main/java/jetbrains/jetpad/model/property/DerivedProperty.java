dbe9b0cdf5de428a65d3024982009b59ff87a75d
Gleb Leonov <Gleb.Leonov@jetbrains.com>
2016-05-03 18:57:15 +0300

    add constructor with initialValue parameter to DerivedProperty class

--- model/src/main/java/jetbrains/jetpad/model/property/DerivedProperty.java
+++ model/src/main/java/jetbrains/jetpad/model/property/DerivedProperty.java
@@ -27,7 +27,11 @@ public abstract class DerivedProperty<ValueT> extends BaseDerivedProperty<ValueT
   private ReadableProperty<?>[] myDeps;
 
   public DerivedProperty(ReadableProperty<?>... deps) {
-    super(null);
+    this(null, deps);
+  }
+
+  public DerivedProperty(ValueT initialValue, ReadableProperty<?>... deps) {
+    super(initialValue);
     myDeps = new ReadableProperty<?>[deps.length];
     System.arraycopy(deps, 0, myDeps, 0, deps.length);
   }
