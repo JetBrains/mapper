d94fd5f1dfe159786e63ec93e25c4d005b0b0049
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2016-05-05 11:19:59 -0400

    comment for NavComposite

--- model/src/main/java/jetbrains/jetpad/model/composite/NavComposite.java
+++ model/src/main/java/jetbrains/jetpad/model/composite/NavComposite.java
@@ -15,6 +15,17 @@
  */
 package jetbrains.jetpad.model.composite;
 
+/**
+ * Composite which supports optimized navigation between siblings.
+ *
+ * If we store children in an array and a composite has a lot of children, navigation might be
+ * expensive because we need to use indexOf which takes O(children). This class is an optimized
+ * version of Composite tailored for such cases.
+ *
+ * Invariants:
+ * - prevSibling, nextSibling, firstChild and lastChild must be consistent with the children collection from
+ * Composite.
+ */
 public interface NavComposite<CompositeT extends NavComposite<CompositeT>> extends Composite<CompositeT> {
   CompositeT nextSibling();
   CompositeT prevSibling();
