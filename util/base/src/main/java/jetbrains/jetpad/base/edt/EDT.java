45ac115f5f12f10153e4c841b9ac963160f27e06
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2016-05-27 16:21:44 -0400

    rename EDT back

--- util/base/src/main/java/jetbrains/jetpad/base/edt/EDT.java
+++ util/base/src/main/java/jetbrains/jetpad/base/edt/EDT.java
@@ -17,7 +17,7 @@ package jetbrains.jetpad.base.edt;
 
 import jetbrains.jetpad.base.Value;
 
-public class Edt {
+public class EDT {
   public static Runnable validateInEdt(final EventDispatchThread validateOn, final Runnable validator) {
     final Value<Boolean> valid = new Value<>(true);
 
