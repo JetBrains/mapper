3217fe99fe50005bb384cd587c93a03f3d6f3957
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2016-05-27 22:12:25 -0400

    start work on event stream API

---
+++ model/src/main/java/jetbrains/jetpad/model/event/stream/EventStream.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2012-2016 JetBrains s.r.o
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrains.jetpad.model.event.stream;
+
+import com.google.common.base.Function;
+import jetbrains.jetpad.base.Registration;
+import jetbrains.jetpad.model.event.EventSource;
+import jetbrains.jetpad.model.event.EventSources;
+import jetbrains.jetpad.model.event.Listeners;
+
+/**
+ * This class is inspired by different implementations of observable/event streams, including: Rx, RxJava, bacon.js,
+ * Dart Stream API, etc.
+ *
+ * RxJava doesn't work in GWT, but is used heavily in Java code so we class names were chosen to be distinct from
+ * classes there to avoid naming conflicts.
+ */
+public final class EventStream<EventT> {
+  private EventSource<EventStreamItem<EventT>> myEventSource;
+  private Listeners<EventStreamListener<EventT>> myListeners = new Listeners<>();
+
+  public EventStream(EventSource<EventStreamItem<EventT>> eventSource) {
+    myEventSource = eventSource;
+  }
+
+  public<TargetEventT> EventStream<TargetEventT> map(final Function<? super EventT, ? extends TargetEventT> f) {
+    return new EventStream<>(EventSources.map(myEventSource, new Function<EventStreamItem<EventT>, EventStreamItem<TargetEventT>>() {
+      @Override
+      public EventStreamItem<TargetEventT> apply(EventStreamItem<EventT> input) {
+        if (input.isEvent()) {
+          return EventStreamItem.event(f.apply(input.getEvent()));
+        } else if (input.isError()) {
+          return EventStreamItem.error(input.getError());
+        } else {
+          return EventStreamItem.finalItem();
+        }
+      }
+    }));
+  }
+
+  public Registration addListener(EventStreamListener<EventT> l) {
+    return myListeners.add(l);
+  }
+}
---
+++ model/src/main/java/jetbrains/jetpad/model/event/stream/EventStreamItem.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2012-2016 JetBrains s.r.o
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrains.jetpad.model.event.stream;
+
+import jetbrains.jetpad.model.event.ListenerEvent;
+
+public final class EventStreamItem<EventT> implements ListenerEvent<EventStreamListener<EventT>> {
+  public static <EventT> EventStreamItem<EventT> event(EventT event) {
+    return new EventStreamItem<>(event, null);
+  }
+
+  public static <EventT> EventStreamItem<EventT> error(Throwable t) {
+    return new EventStreamItem<>(null, t);
+  }
+
+  public static <EventT> EventStreamItem<EventT> finalItem() {
+    return new EventStreamItem<>(null, null);
+  }
+
+  private EventT myEvent;
+  private Throwable myError;
+
+  private EventStreamItem(EventT event, Throwable error) {
+    if (event != null && error != null) {
+      throw new IllegalArgumentException();
+    }
+
+    myEvent = event;
+    myError = error;
+  }
+
+  public EventT getEvent() {
+    return myEvent;
+  }
+
+  public Throwable getError() {
+    return myError;
+  }
+
+  public boolean isFinalItem() {
+    return myError == null && myEvent == null;
+  }
+
+  public boolean isEvent() {
+    return myEvent != null;
+  }
+
+  public boolean isError() {
+    return myError != null;
+  }
+
+  @Override
+  public void dispatch(EventStreamListener<EventT> l) {
+    if (isError()) {
+      l.onEvent(myEvent);
+    } else if (isError()) {
+      l.onError(myError);
+    } else {
+      l.onEnd();
+    }
+  }
+}
