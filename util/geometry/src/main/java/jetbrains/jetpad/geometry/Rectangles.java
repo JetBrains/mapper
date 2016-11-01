1fec88ab7e2637472998d3f7cbdaab6c8700dc2e
Aleksei Berezkin <aleksei.berezkin@jetbrains.com>
2016-11-01 19:23:51 -0400

    Untroduced rectangles util

---
+++ util/geometry/src/main/java/jetbrains/jetpad/geometry/Rectangles.java
@@ -0,0 +1,70 @@
+package jetbrains.jetpad.geometry;
+
+/**
+ * X axis positive direction is right;
+ * Y axis positive direction is down.
+ */
+public class Rectangles {
+  public static Rectangle zeroOrigin(Rectangle r) {
+    return new Rectangle(Vector.ZERO, r.dimension);
+  }
+
+  public static int upperDistance(Rectangle inner, Rectangle outer) {
+    assertOuterInner(outer, inner);
+    return topLeft(inner).y - topLeft(outer).y;
+  }
+
+  public static int lowerDistance(Rectangle inner, Rectangle outer) {
+    assertOuterInner(outer, inner);
+    return bottomLeft(outer).y - bottomLeft(inner).y;
+  }
+
+  public static int leftDistance(Rectangle inner, Rectangle outer) {
+    assertOuterInner(outer, inner);
+    return topLeft(inner).x - topLeft(outer).x;
+  }
+
+  public static int rightDistance(Rectangle inner, Rectangle outer) {
+    assertOuterInner(outer, inner);
+    return topRight(outer).x - topRight(inner).x;
+  }
+
+  private static void assertOuterInner(Rectangle outer, Rectangle inner) {
+    if (!outer.contains(inner)) {
+      throw new IllegalArgumentException("Wrong args");
+    }
+  }
+
+  public static Rectangle extendUp(Rectangle r, int distance) {
+    Vector change = new Vector(0, distance);
+    return new Rectangle(r.origin.sub(change), r.dimension.add(change));
+  }
+
+  public static Rectangle extendDown(Rectangle r, int distance) {
+    return r.changeDimension(r.dimension.add(new Vector(0, distance)));
+  }
+
+  public static Rectangle extendLeft(Rectangle r, int distance) {
+    Vector change = new Vector(distance, 0);
+    return new Rectangle(r.origin.sub(change), r.dimension.add(change));
+  }
+
+  public static Rectangle extendRight(Rectangle r, int distance) {
+    return r.changeDimension(r.dimension.add(new Vector(distance, 0)));
+  }
+
+  private static Vector topLeft(Rectangle r) {
+    return r.origin;
+  }
+
+  private static Vector topRight(Rectangle r) {
+    return r.origin.add(new Vector(r.dimension.x, 0));
+  }
+
+  private static Vector bottomLeft(Rectangle r) {
+    return r.origin.add(new Vector(0, r.dimension.y));
+  }
+
+  private Rectangles() {
+  }
+}
---
+++ util/geometry/src/test/java/jetbrains/jetpad/geometry/RectanglesTest.java
@@ -0,0 +1,70 @@
+package jetbrains.jetpad.geometry;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RectanglesTest {
+  private static final Rectangle INNER = new Rectangle(10, 20, 30, 40);
+  private static final Rectangle OUTER = new Rectangle(0, 5, 100, 200);
+
+  @Test
+  public void upperDistance() {
+    assertEquals(15, Rectangles.upperDistance(INNER, OUTER));
+  }
+
+  @Test
+  public void lowerDistance() {
+    assertEquals(145, Rectangles.lowerDistance(INNER, OUTER));
+  }
+
+  @Test
+  public void leftDistance() {
+    assertEquals(10, Rectangles.leftDistance(INNER, OUTER));
+  }
+
+  @Test
+  public void rightDistance() {
+    assertEquals(60, Rectangles.rightDistance(INNER, OUTER));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void badUpperDistance() {
+    Rectangles.upperDistance(OUTER, INNER);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void badLowerDistance() {
+    Rectangles.lowerDistance(OUTER, INNER);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void badLeftDistance() {
+    Rectangles.leftDistance(OUTER, INNER);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void badRightDistance() {
+    Rectangles.rightDistance(OUTER, INNER);
+  }
+
+  @Test
+  public void extendUp() {
+    assertEquals(new Rectangle(new Vector(10, 19), new Vector(30, 41)), Rectangles.extendUp(INNER, 1));
+  }
+
+  @Test
+  public void extendDown() {
+    assertEquals(new Rectangle(new Vector(10, 20), new Vector(30, 41)), Rectangles.extendDown(INNER, 1));
+  }
+
+  @Test
+  public void extendLeft() {
+    assertEquals(new Rectangle(new Vector(9, 20), new Vector(31, 40)), Rectangles.extendLeft(INNER, 1));
+  }
+
+  @Test
+  public void extendRight() {
+    assertEquals(new Rectangle(new Vector(10, 20), new Vector(31, 40)), Rectangles.extendRight(INNER, 1));
+  }
+}
