0e466710071e4db9fa3533cd87d854a9b57b1126
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2016-11-03 16:31:44 -0400

    prefix in system properties provider

--- util/base/src/main/java/jetbrains/jetpad/base/props/SystemPropertyProvider.java
+++ util/base/src/main/java/jetbrains/jetpad/base/props/SystemPropertyProvider.java
@@ -1,8 +1,14 @@
 package jetbrains.jetpad.base.props;
 
 public class SystemPropertyProvider implements PropertyProvider {
+  private String myPrefix;
+
+  public SystemPropertyProvider(String myPrefix) {
+    this.myPrefix = myPrefix;
+  }
+
   @Override
   public String get(String key) {
-    return System.getProperty(key);
+    return System.getProperty(myPrefix + "." + key);
   }
 }
