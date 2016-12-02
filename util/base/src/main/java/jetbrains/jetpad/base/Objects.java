a36513565c5c4e76a1b21560e1d65f98598add26
Konstantin Solomatov <Konstantin.Solomatov@gmail.com>
2016-12-02 14:25:53 -0500

    remove string comparison hack

--- util/base/src/main/java/jetbrains/jetpad/base/Objects.java
+++ util/base/src/main/java/jetbrains/jetpad/base/Objects.java
@@ -16,26 +16,10 @@
 package jetbrains.jetpad.base;
 
 public class Objects {
-  /**
-   * This method is added to replace com.google.common.base.Objects.equal(). The latter sometimes performs about 100 ms
-   * on two equal 80K strings in Chrome 49.0.2623.87
-   */
   public static boolean equal(Object o1, Object o2) {
     if (o1 == null) return o2 == null;
     if (o2 == null) return false;
-
-    if (o1 instanceof String && o2 instanceof String) {
-      String s1 = (String) o1;
-      String s2 = (String) o2;
-      if (s1.length() != s2.length()) return false;
-      for (int i = 0; i < s1.length(); i++) {
-        if (s1.charAt(i) != s2.charAt(i)) {
-          return false;
-        }
-      }
-      return true;
-    }
-
+    if (o1 == o2) return true;
     return o1.equals(o2);
   }
 }
