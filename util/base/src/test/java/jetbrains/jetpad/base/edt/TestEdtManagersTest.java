5556ae8d23c5517cc934631fe99ee93f5126444e
Gleb Leonov <Gleb.Leonov@jetbrains.com>
2016-12-20 21:43:11 +0300

    introduce TestEdtManagers class to simplify work with multiple edt managers in tests

---
+++ util/base/src/test/java/jetbrains/jetpad/base/edt/TestEdtManagersTest.java
@@ -0,0 +1,96 @@
+package jetbrains.jetpad.base.edt;
+
+import jetbrains.jetpad.test.BaseTestCase;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class TestEdtManagersTest extends BaseTestCase {
+  private TestEdtManagers managers = new TestEdtManagers();
+
+  @Test
+  public void managerRemovedOnFinish() {
+    TestEdtManager manager = managers.createEdtManager("a");
+    manager.finish();
+
+    assertTrue(managers.getManagers().isEmpty());
+  }
+
+  @Test
+  public void managerRemovedOnKill() {
+    TestEdtManager manager = managers.createEdtManager("a");
+    manager.kill();
+
+    assertTrue(managers.getManagers().isEmpty());
+  }
+
+  @Test
+  public void managerByFullName() {
+    String name = "abc";
+    managers.createEdtManager(name);
+
+    assertEquals(name, managers.getEdt(name).getName());
+  }
+
+  @Test
+  public void managerByNamePart() {
+    String name = "abc";
+    managers.createEdtManager(name);
+
+    assertEquals(name, managers.getEdt("b").getName());
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void noManagerByName() {
+    managers.getManager("no manager");
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void multipleManagersByNamePart() {
+    managers.createEdtManager("ab");
+    managers.createEdtManager("ac");
+
+    managers.getManager("a");
+  }
+
+  @Test
+  public void eventsOrder() {
+    final List<Integer> order = new ArrayList<>();
+
+    final TestEventDispatchThread a = managers.createEdtManager("a").getEdt();
+    final TestEventDispatchThread b = managers.createEdtManager("b").getEdt();
+
+    b.schedule(new Runnable() {
+      @Override
+      public void run() {
+        order.add(1);
+        a.schedule(new Runnable() {
+          @Override
+          public void run() {
+            order.add(2);
+          }
+        });
+      }
+    });
+    a.schedule(10, new Runnable() {
+      @Override
+      public void run() {
+        order.add(3);
+        b.schedule(new Runnable() {
+          @Override
+          public void run() {
+            order.add(4);
+          }
+        });
+      }
+    });
+
+    managers.flush(10);
+    assertEquals(Arrays.asList(1, 2, 3, 4), order);
+  }
+}
