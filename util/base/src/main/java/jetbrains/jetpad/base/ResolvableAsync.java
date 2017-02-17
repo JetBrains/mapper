7f4c6f08d15910328a4ff0d759fb53ff3a5e8e8e
Stepan Tarasevich <stepan.tarasevich@jetbrains.com>
2017-02-17 17:36:04 +0300

    Make ResolvableAsync package-protected

--- util/base/src/main/java/jetbrains/jetpad/base/ResolvableAsync.java
+++ util/base/src/main/java/jetbrains/jetpad/base/ResolvableAsync.java
@@ -1,4 +1,4 @@
 package jetbrains.jetpad.base;
 
-public interface ResolvableAsync<ItemT> extends Async<ItemT>, AsyncResolver<ItemT> {
+interface ResolvableAsync<ItemT> extends Async<ItemT>, AsyncResolver<ItemT> {
 }
