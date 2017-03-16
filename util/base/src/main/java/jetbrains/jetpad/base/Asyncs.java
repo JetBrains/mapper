dd2ca6dd3d791354da7f8035077ed52b7311f6bb
Stepan Tarasevich <stepan.tarasevich@jetbrains.com>
2017-03-16 10:40:49 +0300

    Add Asyncs.threadSafeParallel

--- util/base/src/main/java/jetbrains/jetpad/base/Asyncs.java
+++ util/base/src/main/java/jetbrains/jetpad/base/Asyncs.java
@@ -24,11 +24,13 @@ import jetbrains.jetpad.base.function.Supplier;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 @GwtCompatible
@@ -188,15 +190,22 @@ public class Asyncs {
     return parallel(asyncs, false);
   }
 
-  public static Async<Void> parallel(Collection<? extends Async<?>> asyncs, final boolean alwaysSucceed) {
-    final SimpleAsync<Void> result = new SimpleAsync<>();
-    final Value<Integer> inProgress = new Value<>(asyncs.size());
-    final List<Throwable> exceptions = new ArrayList<>();
+  public static Async<Void> parallel(Collection<? extends Async<?>> asyncs, boolean alwaysSucceed) {
+    return parallel(asyncs, alwaysSucceed, new SimpleParallelData(asyncs.size()));
+  }
 
+  public static Async<Void> threadSafeParallel(Collection<? extends Async<?>> asyncs) {
+    return parallel(asyncs, false, new ThreadSafeParallelData(asyncs.size()));
+  }
+
+  public static Async<Void> parallel(Collection<? extends Async<?>> asyncs, final boolean alwaysSucceed,
+      final ParallelData parallelData) {
+    final ResolvableAsync<Void> result = parallelData.getResultAsync();
     final Runnable checkTermination = new Runnable() {
       @Override
       public void run() {
-        if (inProgress.get() == 0) {
+        if (parallelData.decrementInProgressAndGet() == 0) {
+          List<Throwable> exceptions = parallelData.getExceptions();
           if (!exceptions.isEmpty() && !alwaysSucceed) {
             result.failure(new ThrowableCollectionException(exceptions));
           } else {
@@ -210,22 +219,20 @@ public class Asyncs {
       a.onResult(new Consumer<Object>() {
           @Override
           public void accept(Object item) {
-            inProgress.set(inProgress.get() - 1);
             checkTermination.run();
           }
         },
         new Consumer<Throwable>() {
           @Override
           public void accept(Throwable failure) {
-            exceptions.add(failure);
-            inProgress.set(inProgress.get() - 1);
+            parallelData.addException(failure);
             checkTermination.run();
           }
         });
     }
 
     if (asyncs.isEmpty()) {
-      checkTermination.run();
+      return Asyncs.constant(null);
     }
 
     return result;
@@ -453,6 +460,63 @@ public class Asyncs {
               async.failure(failure);
             }
           });
+    }
+  }
+
+  private static abstract class ParallelData {
+    private final List<Throwable> myExceptions;
+    private final ResolvableAsync<Void> myResultAsync;
+
+    ParallelData(List<Throwable> expectionsList, ResolvableAsync<Void> resultAsync) {
+      myExceptions = expectionsList;
+      myResultAsync = resultAsync;
+    }
+
+    /**
+     * Decreases amount of asyncs in progress by one and returns remaining amount
+     */
+    abstract int decrementInProgressAndGet();
+
+    void addException(Throwable t) {
+      myExceptions.add(t);
+    }
+
+    List<Throwable> getExceptions() {
+      return myExceptions;
+    }
+
+    ResolvableAsync<Void> getResultAsync() {
+      return myResultAsync;
+    }
+  }
+
+  private static class SimpleParallelData extends ParallelData {
+    private final Value<Integer> myInProgress;
+
+    SimpleParallelData(int asyncsCount) {
+      super(new ArrayList<Throwable>(), new SimpleAsync<Void>());
+      myInProgress = new Value<>(asyncsCount);
+    }
+
+    @Override
+    int decrementInProgressAndGet() {
+      int decreasedValue = myInProgress.get() - 1;
+      myInProgress.set(decreasedValue);
+      return decreasedValue;
+    }
+  }
+
+  private static class ThreadSafeParallelData extends ParallelData {
+    private final AtomicInteger myInProgress;
+
+    ThreadSafeParallelData(int asyncsCount) {
+      super(Collections.synchronizedList(new ArrayList<Throwable>()), new ThreadSafeAsync<Void>());
+      myInProgress = new AtomicInteger(asyncsCount);
+    }
+
+    @Override
+    int decrementInProgressAndGet() {
+      return myInProgress.decrementAndGet();
     }
   }
 }
