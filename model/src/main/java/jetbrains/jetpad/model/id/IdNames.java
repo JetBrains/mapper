4f8d3ecae62480f14546f951201c80a936c9bf91
Alexey Zlobin <alexey.zlobin@jetbrains.com>
2017-07-11 19:30:49 +0300

    guava-gwt based fix for gtw-servlet dependency

--- model/src/main/java/jetbrains/jetpad/model/id/IdNames.java
+++ model/src/main/java/jetbrains/jetpad/model/id/IdNames.java
@@ -1,9 +1,23 @@
 package jetbrains.jetpad.model.id;
 
-interface IdNames {
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+class IdNames {
+  private final ConcurrentMap<String, String> myMap = new ConcurrentHashMap<>();
+
   /**
    * @throws IllegalStateException if the id already has another name.
    */
-  void save(String id, String name);
-  String get(String id);
+  void save(String id, String name) {
+    String oldName = myMap.putIfAbsent(id, name);
+    if (oldName != null && !oldName.equals(name)) {
+      throw new IllegalStateException(
+          "Different name for known id " + id + ", first name = " + oldName + ", name = " + name + "]");
+    }
+  }
+
+  String get(String id) {
+    return myMap.get(id);
+  }
 }
