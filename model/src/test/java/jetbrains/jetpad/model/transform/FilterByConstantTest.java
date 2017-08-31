f78e987d3ccc604087110a3b32d57b95f871d025
Timur Zambalayev <timur@jetbrains.com>
2017-08-31 13:10:24 -0400

    fix "usage of obsolete 'junit.framework.Assert' method" warnings

--- model/src/test/java/jetbrains/jetpad/model/transform/FilterByConstantTest.java
+++ model/src/test/java/jetbrains/jetpad/model/transform/FilterByConstantTest.java
@@ -23,8 +23,8 @@ import jetbrains.jetpad.model.collections.set.ObservableSet;
 import org.junit.Before;
 import org.junit.Test;
 
-import static junit.framework.TestCase.assertFalse;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class FilterByConstantTest {
