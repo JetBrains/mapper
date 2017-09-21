29c95144fc6a426902b307de11a09b09b606243f
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2017-09-21 17:24:41 -0400

    typed key hash map

---
+++ model/src/main/java/jetbrains/jetpad/model/util/TypedKeyHashMap.java
@@ -0,0 +1,19 @@
+package jetbrains.jetpad.model.util;
+
+import java.util.HashMap;
+
+public class TypedKeyHashMap extends HashMap<TypedKey<?>, Object> implements TypedKeyContainer {
+  @Override
+  public <T> T get(TypedKey<T> key) {
+    @SuppressWarnings("unchecked")
+    T result = (T) super.get(key);
+    return result;
+  }
+
+  @Override
+  public <T> T put(TypedKey<T> key, T value) {
+    @SuppressWarnings("unchecked")
+    T result = (T) super.put(key, value);
+    return result;
+  }
+}
