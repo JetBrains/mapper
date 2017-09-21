11396cc4f3c7c6a72fbf4408d41b46b73242da57
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2017-09-21 17:19:42 -0400

    cleaner code

--- model/src/main/java/jetbrains/jetpad/model/util/TypedKeyListMap.java
+++ model/src/main/java/jetbrains/jetpad/model/util/TypedKeyListMap.java
@@ -1,15 +1,17 @@
 package jetbrains.jetpad.model.util;
 
 public class TypedKeyListMap extends ListMap<TypedKey<?>, Object> implements TypedKeyContainer {
-  @SuppressWarnings("unchecked")
   @Override
   public <T> T get(TypedKey<T> key) {
-    return (T) super.get(key);
+    @SuppressWarnings("unchecked")
+    T result = (T) super.get(key);
+    return result;
   }
 
-  @SuppressWarnings("unchecked")
   @Override
   public <T> T put(TypedKey<T> key, T value) {
-    return (T) super.put(key, value);
+    @SuppressWarnings("unchecked")
+    T result = (T) super.put(key, value);
+    return result;
   }
 }
